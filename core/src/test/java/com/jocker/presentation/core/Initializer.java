3294c799b21d15f83e42bf319a0ed632bbcdb492
allaprotopopova <aprotopopova@luxoft.com>
2020-09-17 16:42:31 +0300

    add schema props

--- core/src/test/java/com/jocker/presentation/core/Initializer.java
+++ core/src/test/java/com/jocker/presentation/core/Initializer.java
@@ -1,18 +1,21 @@
 package com.jocker.presentation.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
+@TestPropertySource(locations = "test.properties")
 public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     private static final OracleContainer oracleContainer = new OracleContainer();
 
+
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-
         oracleContainer
                 .withStartupTimeoutSeconds(90)
                 .withConnectTimeoutSeconds(90)
@@ -20,7 +23,7 @@ public class Initializer implements ApplicationContextInitializer<ConfigurableAp
                 .waitingFor(Wait.forLogMessage("Database ready to use", 1));
         oracleContainer.start();
 
-        //String jdbcUrl = "jdbc:oracle:thin:" + oracleContainer.getUsername() + "/" + oracleContainer.getPassword() + "@" + oracleContainer.getContainerIpAddress() + ":" + oracleContainer.getOraclePort() + ":ee";
+        String jdbcUrl = "jdbc:oracle:thin:" + oracleContainer.getUsername() + "/" + oracleContainer.getPassword() + "@" + oracleContainer.getContainerIpAddress() + ":" + oracleContainer.getOraclePort() + ":ee";
         TestPropertyValues.of(
                 "spring.datasource.driver-class-name=" + oracleContainer.getDriverClassName(),
                 "spring.datasource.url=" + oracleContainer.getJdbcUrl()
